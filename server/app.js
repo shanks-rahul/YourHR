63afcff0f9e366a9098a5f18a4115833a307ff52
shanks-rahul <rahulagarwalla1234@gmail.com>
2024-08-29 02:11:01 +0530

    updated the localhost for frontend

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 app.use(
     cors({
-        origin:[process.env.FRONTEND_URL],
+        origin:["http://localhost:5174"],
         credentials:true
     })
 )
