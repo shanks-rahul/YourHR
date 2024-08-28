b5224109c4b74cf12857c9cfc39980e16b5082e2
shanks-rahul <rahulagarwalla1234@gmail.com>
2024-08-29 02:59:52 +0530

    added the new urls

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 app.use(
     cors({
-        origin:["http://localhost:5174"],
+        origin:["https://thunderous-kashata-e956c1.netlify.app/"],
         credentials:true
     })
 )
